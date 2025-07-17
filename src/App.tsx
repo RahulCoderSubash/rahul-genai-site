4fcec631b9452166d4eb9ada3ca3b3b1d0b2eaca
RahulCoderSubash <rahulsubash2501@gmail.com>
2025-07-17 17:08:10 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Rahul Subash - AI Developer</h1>
+      <p>Welcome to my GenAI learning lab and portfolio!</p>
+    </div>
+  );
+}
+
+export default App;
